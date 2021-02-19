6667ab62eeb80d5afb5ba47361ef7c895d3a4ad5
ooya sora <sora-110507@ezweb.ne.jp>
2021-02-19 13:06:18 +0900

    Initial Commit

---
+++ Ra-menApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ra-menApp
+//
+//  Created by 大谷空 on 2021/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
